8380582e999e814357c458d7f2c6867973b2cd63
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:01:35 +0530

    Create changing the character string c++.c

---
+++ changing the character string c++.c
@@ -0,0 +1,13 @@
+// changing the string character by accessing the new characters
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string mystring = "Hello";
+    mystring [0]='k';
+    cout << mystring;
+    return 0;
+}
